5333c19be0d2ffeb28e0cf2295330ad707444b38
bahagh <73429122+bahagh@users.noreply.github.com>
2023-12-23 01:09:52 +0100

    enhancemnts

--- Models/Books.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApplication2.Models
-{
-    public class Books
-    {
-        public string Titles { get; set; }
-        public string Text { get; set; }
-    }
-}
--- wwwroot/app.js
+++ wwwroot/app.js
@@ -1,8 +1,6 @@
 async function createBook() {
     const content = document.getElementById('content').value;
     const titles = document.getElementById('titles').value;
-
-    
     const data = {
         content: content,
         titles: titles
@@ -11,9 +9,9 @@ async function createBook() {
     const response = await fetch('/Books', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json', 
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data), 
+        body: JSON.stringify(data),
     });
 
     if (response.ok) {
@@ -28,23 +26,23 @@ async function getTitles() {
     const titles = await response.json();
 
     const titlesList = document.getElementById('titlesList');
-    titlesList.innerHTML = titles.map(title => <li>${title}</li>).join('');
+    titlesList.innerHTML = titles.map(title => `<li>${title}</li>`).join('');
 }
 
 async function getTopTenWords() {
     const bookId = document.getElementById('bookId').value;
-    const response = await fetch(/Books/GetTopTenWords / ${ bookId });
+    const response = await fetch(`/Books/GetTopTenWords/${bookId}`);
     const topWords = await response.json();
 
     const topWordsList = document.getElementById('topWordsList');
-    topWordsList.innerHTML = topWords.map(word => <li>${word}</li>).join('');
+    topWordsList.innerHTML = topWords.map(word => `<li>${word}</li>`).join('');
 }
 
 async function checkMatchingWord() {
     const bookId = document.getElementById('checkWordId').value;
     const wordToCheck = document.getElementById('wordToCheck').value;
 
-    const response = await fetch(/Books/IsMatchingWord / ${ bookId } / ${ wordToCheck });
+    const response = await fetch(`/Books/IsMatchingWord/${bookId}/${wordToCheck}`);
     const result = await response.json();
 
     const matchingResult = document.getElementById('matchingResult');
@@ -57,9 +55,9 @@ async function findSubstring() {
     const bookId = document.getElementById('substringId').value;
     const substring = document.getElementById('substring').value;
 
-    const response = await fetch(/Books/FindSubstring / ${ bookId } / ${ substring });
+    const response = await fetch(`/Books/FindSubstring/${bookId}/${substring}`);
     const words = await response.json();
 
     const substringList = document.getElementById('substringList');
-    substringList.innerHTML = words.map(word => <li>${word}</li>).join('');
+    substringList.innerHTML = words.map(word => `<li>${word}</li>`).join('');
 }
