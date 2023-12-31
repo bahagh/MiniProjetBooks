df64e7fd2c36dadba83838288e3253c321c08c25
bahagh <73429122+bahagh@users.noreply.github.com>
2023-12-31 17:58:00 +0100

    removing and pushing a new project with all necessary files

--- Models/BookCreationModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApplication2.Models
-{
-    public class BookCreationModel
-    {
-        public string Content { get; set; }
-        public string Titles { get; set; }
-    }
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-IEA Mini Project Test
